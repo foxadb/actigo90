726a4e1e388178e789552c82cd10f2b240c4e8a6
Samy Amraoui <samy.amraoui@ensimag.grenoble-inp.fr>
2018-01-10 12:51:55 +0100

    Clean CMakeLists

--- test/test_basket_2.cpp
+++ test/test_basket_2.cpp
@@ -5,22 +5,20 @@
 #include <ctime>
 #include <iostream>
 
-#include "pnl/pnl_vector.h"
-
 using namespace std;
 
 
 int main(int argc, char** argv) {
   float strike = 100.0;
-  PnlVect* spotVector = pnl_vect_create_from_scalar(40,100);
-  PnlVect* basketWeights = pnl_vect_create_from_scalar(40,0.025);
-  double maturity = 3;
-  int nbSteps = 5;
   int size = 40;
+  PnlVect* spotVector = pnl_vect_create_from_scalar(size,100);
+  PnlVect* basketWeights = pnl_vect_create_from_scalar(size,0.025);
+  double maturity = 1;
+  int nbSteps = 5;
   Basket *myBasket = new Basket(maturity,nbSteps,size,strike,basketWeights);
   double intresetRate = 0.04879;
-  double rho = 0.0;
-  PnlVect* volatVector = pnl_vect_create_from_scalar(40,0.025);
+  double rho = 0.7;
+  PnlVect* volatVector = pnl_vect_create_from_scalar(size,0.2);
   BlackScholesModel *bsm = new BlackScholesModel(size,intresetRate,rho,volatVector,spotVector);
   PnlRng *rng = pnl_rng_create(PNL_RNG_MERSENNE);
   pnl_rng_sseed(rng, time(NULL));
