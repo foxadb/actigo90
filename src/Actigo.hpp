3c034707c447b24fbf4260d2ad25c5e3e36aa725
Hamza Bernoussi <hamzabernoussi96@gmail.com>
2018-01-16 22:57:08 +0100

    Resolve pricer bug and add Actigo Option

---
+++ src/Actigo.hpp
@@ -0,0 +1,36 @@
+/* 
+ * File:   Actigo.hpp
+ * Author: bernoush
+ *
+ */
+#include "Option.hpp"
+
+#ifndef ACTIGO_H
+#define	ACTIGO_H
+
+/// \brief Classe pour le produit Actigo
+class Actigo : public Option {
+public:
+    
+  double euroStoxSpot_; // spot initial de l'indice euroStoxx.
+  double spUsaSpot_; // spot initial de l'indice S&P 500 Usa.
+  double spAusSpot_; // spot initial de l'indice S&P 200 Aus.
+  PnlVect *semestrialSpot_; // vecteur representant le valeur des indices le semstre courant. 
+  
+  Actigo(double maturity, int nbSteps, int size, double euroStoxSpot, double spotUsa, double spAus);
+
+  double indexPerf(double init, double current);
+
+  double payoff(const PnlMat* path);
+   
+  virtual ~Actigo();
+
+  Actigo();
+
+  Actigo(const Actigo& orig);
+
+private:
+
+};
+
+#endif	/* ACTIGO_H */
