3959a402c4f2744cfe5646629454c322d761b615
Samy Amraoui <samy.amraoui@ensimag.grenoble-inp.fr>
2018-01-17 16:23:52 +0100

    Push the csv data into quote spots

--- src/quote/curl_utils.cpp
+++ src/quote/curl_utils.cpp
@@ -7,19 +7,16 @@
 #include "curl_utils.hpp"
 #include "json.hpp"
 
-using namespace std;
-using namespace nlohmann;
-
 size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp) {
-    ((string*)userp)->append((char*)contents, size * nmemb);
+    ((std::string*)userp)->append((char*)contents, size * nmemb);
     return size * nmemb;
 }
 
-void getCrumbCookie(string url,
-                    string *crumb,
-                    string *cookie) {
+void getCrumbCookie(std::string url,
+                    std::string *crumb,
+                    std::string *cookie) {
     CURL* curl = curl_easy_init();
-    string readBuffer;
+    std::string responseBuffer;
 
     const char* cookieFile = "/tmp/yahoo-finance-cookie";
 
@@ -28,9 +25,9 @@ void getCrumbCookie(string url,
         curl_easy_setopt(curl, CURLOPT_COOKIEJAR,  cookieFile);
         curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
 
-        // Write result into the string buffer
+        // Write result into the buffer
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &responseBuffer);
 
         // Perform the request
         CURLcode res = curl_easy_perform(curl);
@@ -39,80 +36,73 @@ void getCrumbCookie(string url,
         curl_easy_cleanup(curl);
     }
 
-    *crumb = extractCrumb(readBuffer);
+    *crumb = extractCrumb(responseBuffer);
     *cookie = extractCookie(cookieFile);
 }
 
-string extractCrumb(string code) {
+std::string extractCrumb(std::string code) {
     int i = 0;
-    string line;
-    istringstream codestream(code);
-    while (getline(codestream, line) && i < 42) {
+    std::string line;
+    std::istringstream codestream(code);
+    while (std::getline(codestream, line) && i < 42) {
         ++i;
     }
 
     line = line.substr(16, line.size() - 17);
 
-    auto json = json::parse(line);
+    auto json = nlohmann::json::parse(line);
 
-    string crumb = json["context"]["dispatcher"]["stores"]["CrumbStore"]["crumb"];
+    std::string crumb = json["context"]["dispatcher"]["stores"]["CrumbStore"]["crumb"];
     return crumb;
 }
 
-string extractCookie(const char *cookieFile) {
+std::string extractCookie(const char *cookieFile) {
     int i = 0;
-    string line;
-    ifstream file(cookieFile);
+    std::string line;
+    std::ifstream file(cookieFile);
 
     if (file.is_open()) {
-        while (getline(file, line) && i < 4) {
+        while (std::getline(file, line) && i < 4) {
             ++i;
         }
         file.close();
     }
 
-    string cookie = "B=" + line.substr(37);
+    std::string cookie = "B=" + line.substr(37);
     return cookie;
 }
 
-void sendDataRequest(string symbol,
+std::string downloadCsv(std::string symbol,
                      int period1,
                      int period2,
-                     string interval,
-                     string *crumb,
-                     string *cookie) {
-    string url = "https://query1.finance.yahoo.com/v7/finance/download/"
+                     std::string interval,
+                     std::string *crumb,
+                     std::string *cookie) {
+    std::string url = "https://query1.finance.yahoo.com/v7/finance/download/"
             + symbol
-            + "?period1=" + to_string(period1)
-            + "&period2=" + to_string(period2)
+            + "?period1=" + std::to_string(period1)
+            + "&period2=" + std::to_string(period2)
             + "&interval=" + interval
             + "&events=history"
             + "&crumb=" + *crumb;
 
     CURL* curl = curl_easy_init();
-    CURLcode res;
+    std::string responseBuffer;
 
     if (curl) {
         curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
         curl_easy_setopt(curl, CURLOPT_COOKIE, cookie->c_str());
 
+        // Write result into the buffer
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &responseBuffer);
+
         // Perform the request
-        res = curl_easy_perform(curl);
+        CURLcode res = curl_easy_perform(curl);
 
         // Cleanup
         curl_easy_cleanup(curl);
     }
-}
 
-CURLcode getUrlData(string url) {
-    CURL *curl = curl_easy_init();
-    CURLcode result;
-
-    if (curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-        result = curl_easy_perform(curl);
-        curl_easy_cleanup(curl);
-    }
-
-    return result;
+    return responseBuffer;
 }
--- src/quote/main.cpp
+++ src/quote/main.cpp
@@ -4,9 +4,16 @@
 #include "quote.hpp"
 
 int main(int argc, char* argv[]) {
+    // Get the symbol and create the quote
     std::string symbol(argv[1]);
+    Quote* quote = new Quote(symbol);
 
-    Quote quote = Quote(symbol);
+    // Get the historical spots from Yahoo Finance
+    quote->getHistoricalSpots(1504710156, 1516184887, "1wk");
 
-    quote.getHistoricalData(1504710156, 1516184887, "1wk");
+    // Print the spots
+    quote->printSpots();
+
+    // Free memory
+    delete quote;
 }
