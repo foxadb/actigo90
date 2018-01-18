d5b98f687ebdc36ef087ee70ea464a176a608263
Zineb El Filali Ech-Chafiq <zineb.el-filali-ech-chafiq@grenoble-inp.org>
2018-01-18 22:13:08 +0100

    some changes in calibration

--- src/pricer/Calibration.cpp
+++ src/pricer/Calibration.cpp
@@ -7,7 +7,7 @@ using namespace std;
 double Calibration::corr(PnlVect *x, PnlVect *y){
 
   double x_mean, y_mean = 0.0;
-  int size = MIN(x->size, y->size);
+  int size = min(x->size, y->size);
 
   for (int i = 0; i < size; i++){
     x_mean += GET(x, i);
