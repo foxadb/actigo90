ad4862a27441c73d45d28899cc753eeeededc2f3
Hamza Bernoussi <hamzabernoussi96@gmail.com>
2018-01-24 19:46:09 +0100

    Add pricerActigo test

---
+++ src/pricer/pricerActigo.cpp
@@ -0,0 +1,11 @@
+#include "BlackScholesModel.hpp"
+#include "MonteCarlo.hpp"
+#include "Actigo.hpp"
+#include "Option.hpp"
+#include <string>
+#include <iostream>
+
+
+int main(int argc, char **argv){
+  return 0;
+}
