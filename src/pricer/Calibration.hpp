26379392078d052980a990a00735c1c7148b8998
Zineb El Filali Ech-Chafiq <zineb.el-filali-ech-chafiq@grenoble-inp.org>
2018-01-25 03:09:21 +0100

    deleted depricated constructor

--- src/pricer/Calibration.hpp
+++ src/pricer/Calibration.hpp
@@ -15,8 +15,10 @@ public:
   const char *period2;
   double estimate_volatility(PnlVect *x);
   double estimate_correlation(PnlVect *x, PnlVect *y);
+  double estimate_trend(PnlVect *x);
   Calibration(const char *period1, const char *period2);
   Calibration(Data *data);
   PnlVect* volatilities(PnlMat *correlations);
   PnlMat* getCorrelationsMatrix();
+  PnlVect* getTrends();
 };
