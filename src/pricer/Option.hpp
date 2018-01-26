b814313bcb570cb056e04ebfd9fcd5e5c66e46fb
elfilalz <zineb.el-filali-ech-chafiq@grenoble-inp.org>
2018-01-26 18:32:37 +0100

    translated doc to english

--- src/pricer/Option.hpp
+++ src/pricer/Option.hpp
@@ -3,24 +3,23 @@
 #include "pnl/pnl_vector.h"
 #include "pnl/pnl_matrix.h"
 
-/// \brief Classe Option abstraite
+/**
+* @brief Option class
+*/
 class Option {
 public:
-    double T_; /// maturité
-    int nbTimeSteps_; /// nombre de pas de temps de discrétisation
-    int size_; /// dimension du modèle, redondant avec BlackScholesModel::size_
+    double T_; /// maturity
+    int nbTimeSteps_; /// nbre of steps for the finite difference discretization
+    int size_; /// Model dimension
 
     virtual ~Option() {}
 
     /**
-     * Calcule la valeur du payoff sur la trajectoire
+     * @brief computes the payoff on the given path
      *
-     * @param[in] path est une matrice de taille (N+1) x d
-     * contenant une trajectoire du modèle telle que créée
-     * par la fonction asset.
-     * @return phi(trajectoire)
+     * @param[in] path is a matrix of size (N+1) x d
+     * containing a path of the model
+     * @return phi(path)
      */
     virtual double payoff(const PnlMat *path) = 0;
 };
-
-
