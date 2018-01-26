f93e7bdab7bded14ad990e3a06a0d2a4c62f220d
Samy Amraoui <samy.amraoui@ensimag.grenoble-inp.fr>
2018-01-26 19:58:48 +0100

    Add P&L computation timer

--- test/pricerActigo.cpp
+++ test/pricerActigo.cpp
@@ -1,47 +1,59 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+
 #include "BlackScholesModel.hpp"
 #include "MonteCarlo.hpp"
 #include "Actigo.hpp"
 #include "Option.hpp"
 #include "Data.hpp"
 #include "Calibration.hpp"
-#include <string>
-#include "../quote/time_utils.hpp"
-#include <iostream>
 
 using namespace std;
 
+int main(int argc, char **argv) {
+    //Recuperate data from actigo first day: "2015-10-12" to today
+    Data *data = new Data("2015-10-12","2018-01-24");
+    cout << "Historical matrix size: " << data->historicalDataMatrix->m << endl;
 
-int main(int argc, char **argv){
-  //Recuperate data from actigo first day: "2015-10-12" to today
-  Data *data = new Data("2015-10-12","2018-01-24");
-  cout << "size" << data->historicalDataMatrix->m << endl;
-  //Calibrate data
-  // create Actigo Option
-  double maturity = 8.;
-  int nbTimeSteps = 2016;
-  double step = maturity / nbTimeSteps;
-  Calibration *calibration = new Calibration(data, step);
-  int size = 5;
-  PnlVect *initialSpots = pnl_vect_create(size);
-  PnlVect *todaySpots = pnl_vect_create(size);
-  data->getInitialSpots(initialSpots);
-  data->getTodaySpots(todaySpots);
-  Actigo *actigo = new Actigo(maturity, nbTimeSteps, size, GET(initialSpots,0), GET(initialSpots, 1), GET(initialSpots, 2));
-  //complete data from today to actigo end date
-  int remainingDates = nbTimeSteps - data->euroStoxSpots->size;
-  PnlRng *rng = pnl_rng_create(PNL_RNG_MERSENNE);
-  pnl_rng_sseed(rng, time(NULL));
-  data->completeData(remainingDates, actigo, todaySpots, calibration->getVolatilities(),
-                    calibration->getCorrelationsMatrix(), calibration->getTrends(), rng);
-  //create the BlackScholesModel
-  double rEur = 0.04;
-  BlackScholesModel *bsm = new BlackScholesModel(size, rEur, calibration->getCorrelationsMatrix(), calibration->getVolatilities(), initialSpots);
-  //create monteCarlo Simulation
-  int nbSamples = 50000;
-  double fdStep = 0.01;
+    //// Data calibration
+    // Create Actigo Option
+    double maturity = 8;
+    int nbTimeSteps = 2016;
+    double step = maturity / nbTimeSteps;
+    Calibration *calibration = new Calibration(data, step);
+    int size = 5;
+    PnlVect *initialSpots = pnl_vect_create(size);
+    PnlVect *todaySpots = pnl_vect_create(size);
+    data->getInitialSpots(initialSpots);
+    data->getTodaySpots(todaySpots);
+    Actigo *actigo = new Actigo(maturity, nbTimeSteps, size, GET(initialSpots,0), GET(initialSpots, 1), GET(initialSpots, 2));
 
-  MonteCarlo *mc = new MonteCarlo(bsm, actigo, rng, fdStep, nbSamples);
-  double pl = mc->pAndL(data->completeDataMatrix);
-  cout << " the P&L is " << pl << endl;
-  return 0;
+    // Complete data from today to actigo end date
+    int remainingDates = nbTimeSteps - data->euroStoxSpots->size;
+    PnlRng *rng = pnl_rng_create(PNL_RNG_MERSENNE);
+    pnl_rng_sseed(rng, time(NULL));
+    data->completeData(remainingDates, actigo, todaySpots, calibration->getVolatilities(),
+                       calibration->getCorrelationsMatrix(), calibration->getTrends(), rng);
+
+    // Create the BlackScholesModel
+    double rEur = 0.04;
+    BlackScholesModel *bsm = new BlackScholesModel(size, rEur, calibration->getCorrelationsMatrix(), calibration->getVolatilities(), initialSpots);
+
+    // Create Monte-Carlo Simulation
+    int nbSamples = atoi(argv[1]);
+    double fdStep = 0.01;
+
+    cout << "nbSamples: " << nbSamples << endl;
+
+    MonteCarlo *mc = new MonteCarlo(bsm, actigo, rng, fdStep, nbSamples);
+
+    // Compute P&L
+    clock_t pnlStart = clock();
+    double pnl = mc->pAndL(data->completeDataMatrix);
+    double pnlDuration = (clock() - pnlStart) / (double)CLOCKS_PER_SEC;
+    cout << "P&L: " << pnl * 100 << "%" << endl;
+    cout << "P&L duration: " << pnlDuration << " seconds" << endl;
+
+    return EXIT_SUCCESS;
 }
