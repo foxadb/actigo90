175fadcb0262e87197e318d6f2efcf927956945e
Zineb El Filali Ech-Chafiq <zineb.el-filali-ech-chafiq@grenoble-inp.org>
2018-03-17 20:15:18 +0100

    test for dbManager:post_delta and get_delta

--- pricer/test/dbManagerTest.cpp
+++ pricer/test/dbManagerTest.cpp
@@ -6,8 +6,13 @@
 int main(int argc, char **argv){
   DataBaseManager *dbManager = DataBaseManager::getDbManager();
   Spot spot = dbManager->getSpot("2017-01-19", "S&P 500");
-  std::cout<<spot.toString()<<std::endl; 
+  std::cout<<spot.toString()<<std::endl;
+
   std::vector<Spot> spots = dbManager->getSpots("2017-01-19", "2017-01-20", "S&P 500");
   for (std::vector<Spot>::iterator it = spots.begin(); it != spots.end(); ++it)
-    std::cout << ' ' << (*it).toString();
+    std::cout << ' ' << (*it).toString()<<std::endl;
+
+  dbManager->post_delta(0.05, "2017-01-19", "S&P 500");
+  double delta = dbManager->get_delta("2017-01-19", "S&P 500");
+  std::cout << delta << std::endl;
 }
