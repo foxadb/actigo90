801054d2c302f7e1c07f44b8f9864b4794408ccd
Zineb El Filali Ech-Chafiq <zineb.el-filali-ech-chafiq@grenoble-inp.org>
2018-03-17 21:27:27 +0100

    added google tests

---
+++ pricer/test/quote/DataBaseManagerTest.cpp
@@ -0,0 +1,28 @@
+#include "gtest/gtest.h"
+#include "DataBaseManager.hpp"
+
+using namespace std;
+
+class DataBaseManagerTest : public ::testing::Test {
+    protected:
+
+        // Tolerance for double equality test
+        double abs_error = 10e-5;
+
+        virtual void SetUp() {
+            // code called before each test (beginning)
+        }
+
+        virtual void TearDown() {
+            // code called before each test destructor (ending)
+        }
+};
+
+TEST_F(DataBaseManagerTest, get_delta) {
+  DataBaseManager *dbManager = DataBaseManager::getDbManager();
+  double posted_delta = 0.02;
+  dbManager->post_delta(posted_delta, "2018-01-01", "S&P 500");
+  double returned_delta = dbManager->get_delta("2018-01-01", "S&P 500");
+
+  EXPECT_EQ(posted_delta, returned_delta);
+}
