a3451ef6b528bbea5f131ace43f18849a2d1c048
Hamza Bernoussi <hamzabernoussi96@gmail.com>
2018-03-22 00:01:59 +0100

    modifyin some tests

--- pricer/test/computeActigoDelta.cpp
+++ pricer/test/computeActigoDelta.cpp
@@ -95,12 +95,13 @@ std::vector<time_t> semesterDates {1428451200, 1444608000, 1460332800, 147605760
                                             calibration->getVolatilities(), initialSpotsEuro);
   PnlRng *rng = pnl_rng_create(PNL_RNG_MERSENNE);
   pnl_rng_sseed(rng, time(NULL));
-  MonteCarlo *mc = new MonteCarlo(bsm, actigo, rng, 0.01, 50000);
+  MonteCarlo *mc = new MonteCarlo(bsm, actigo, rng, 0.01, 5);
   PnlVect *delta = pnl_vect_create_from_scalar(actigoSize, 0.);
   double price = 0.;
   std::vector<time_t> rightDates = getRightDates(date, semesterDates);
   PnlMat* past = pnl_mat_create_from_scalar(rightDates.size(), actigoSize, 0.);
   getPastData(dbManager, past, rightDates);
+  pnl_mat_print(past);
 
   time_t dateDifference = date - 1428451200;
   double convertedDate = (double)dateDifference/(365*24*3600);
--- pricer/test/dbManagerTest.cpp
+++ pricer/test/dbManagerTest.cpp
@@ -6,10 +6,10 @@
 
 int main(int argc, char **argv){
   DataBaseManager *dbManager = DataBaseManager::getDbManager();
-  Spot spot = dbManager->getSpot(1484784000, "^GSPC");
+  Spot spot = dbManager->getSpot(1520553600, "^GSPC");
   std::cout<<spot.toString()<<std::endl;
 
-  std::vector<Spot> spots = dbManager->getSpots(1453161600, 1484784000, "^GSPC");
+  /*std::vector<Spot> spots = dbManager->getSpots(1453161600, 1484784000, "^GSPC");
   for (std::vector<Spot>::iterator it = spots.begin(); it != spots.end(); ++it)
-    std::cout << ' ' << (*it).toString()<<std::endl;
+    std::cout << ' ' << (*it).toString()<<std::endl;*/
 }
